3f9c876fadb1f788327c66f84efc8b1d12f3e2aa
SimonMarcell <79018646+SimonMarcell@users.noreply.github.com>
2021-11-30 15:18:06 +0100

    Add files via upload

---
+++ K9IVJV_1130/src/jsongyakorlas/ObjectK9IVJV.java
@@ -0,0 +1,16 @@
+package jsongyakorlas;
+
+import org.json.simple.JSONObject;
+
+public class ObjectK9IVJV {
+
+	public static void main(String[] args) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("Név", "BLászló");
+		jsonObject.put("Fizetes", "1000000");
+		jsonObject.put("Kor", "21");
+
+		System.out.println(jsonObject);
+	}
+
+}
